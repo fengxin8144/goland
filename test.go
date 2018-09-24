1d4c1dcd9fd9f6c2134dd6e44eb002e6e44042c3
阿星 <>
2018-09-24 15:22:03 +0700

    qqq

--- test.go
+++ test.go
@@ -7,11 +7,8 @@ import (
 
 func main() {
 	var src string
-	flag.StringVar(&src, "src", "", "source file")
 	var level *int
-	level = flag.Int("level", 0, "debug level")
 	var memo string
-	flag.StringVar(&memo, "memo", "", "the memory")
 	flag.Parse()
 	flag.Usage()
 	fmt.Printf("src=%s, level=%d, memo=%s\n", src, *level, memo)
